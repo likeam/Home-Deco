36243ebefdcd0ebdaa004c7eccc45d591e6a2c33
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-18 21:37:38 +0500

    Building Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,17 @@ import Products from './pages/ProductsPage';
 import Cart from './pages/CartPage';
 import Checkout from './pages/CheckoutPage';
 import Error from './pages/ErrorPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+
 
 
 export default function App() {
   return (
     <Router>
+      <Navbar />
+      <Sidebar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
@@ -20,7 +26,7 @@ export default function App() {
         <Route path='checkout' element={<Checkout />} />
         <Route path='*' element={<Error />} />
       </Routes>
-
+      <Footer />
     </Router>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className=' h-5 flex items-center justify-center bg-slate-500'>
+        <div className=" h-5 w-[90vw] ">
+          <div className="flex items-center justify-self-end"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
